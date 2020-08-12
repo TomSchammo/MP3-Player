cfe903f78776d1f9349c55330b9c6e09ca6c2405
Tom Schammo <tom@tomschammo.com>
2020-08-12 13:49:51 +0200

    Added main function and minor changes in main.cpp
    - Added main function
    - Prompting the user to enter a file. This is only for tests, this will
    change over time

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+    std::cout << "Please enter the directory of the file that should be played" << std::endl;
+
+    std::string filename;
+
+    std::cin >> filename;
+
+
+    return 0;
+}
