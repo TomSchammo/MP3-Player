cf5f6a7b8691ba89696b11f5acaadd368580aeb3
Tom Schammo <tom@tomschammo.com>
2021-01-10 17:16:08 +0100

    Added important file locations
    Added constants that represent locations in an mp3 file where certain
    parts of the ID3 tag should be located

--- include/id3.hpp
+++ include/id3.hpp
@@ -2,6 +2,17 @@
 #define ID3_HPP
 #include <filehandler.hpp>
 
+// constants
+constexpr unsigned char LOCATION_START = 0;
+constexpr unsigned char LOCATION_VERSION = 3;
+constexpr unsigned char LOCATION_FLAGS = 5;
+constexpr unsigned char LOCATION_SIZE = 6;
+constexpr unsigned char LOCATION_EXTENDED_HEADER = 10;
+constexpr unsigned char SIZE_OF_FLAGS = 1;
+constexpr unsigned char SIZE_OF_VERSION = 1;
+constexpr unsigned char SIZE_OF_SIZE = 4;
+constexpr unsigned char SIZE_OF_FRAME_ID = 4;
+
 
 
 #endif // ID3_HPP
