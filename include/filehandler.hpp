7979b898dcc47e952aec4384d5d4df6c82d3fade
Tom Schammo <tom@tomschammo.com>
2021-01-25 23:40:24 +0100

    Moved documentation into header file

--- include/filehandler.hpp
+++ include/filehandler.hpp
@@ -7,11 +7,49 @@
 class Filehandler {
 
     public:
+
+        /**
+         * Class constructor, initializes filename and opens a read only stream
+         * for the file.
+         *
+         * @param filename name of the file
+         */
         Filehandler(const char* filename);
+
+
+        /**
+         * Checks whether a file exists or not.
+         *
+         * @return true if the file exists, false otherwise.
+         */
         inline bool exists();
+
+
+        /**
+         * Reads "bytes" bytes from a stream, starting at byte "position".
+         *
+         * @param position  The starting position of the pointer
+         * @param bytes     The number of bytes that should be read
+         * @return a pointer to the first byte of a char array containing the requested bytes
+         */
         char* read(const unsigned int position, const unsigned char bytes);
+
+
+        /**
+         * Reads "bytes" bytes from a stream, starting at byte "position".
+         *
+         * @param position  The starting position of the pointer
+         * @param bytes     The number of bytes that should be read
+         * @return a pointer to the first byte of a char array containing the requested bytes
+         */
         char* read(const unsigned int position, std::_Ios_Seekdir way, const unsigned char bytes);
+
+
+        /**
+         * Class destructor, closes the filestream if open.
+         */
         ~Filehandler();
+
 
     private:
         const char* filename;
--- src/filehandler.cpp
+++ src/filehandler.cpp
@@ -1,11 +1,6 @@
 #include <filehandler.hpp>
 
-/**
- * Class constructor, initializes filename and opens a read only stream
- * for the file.
- *
- * @param filename name of the file
- */
+
 Filehandler::Filehandler(const char* filename) {
     this->filename = filename;
 
@@ -15,23 +10,12 @@ Filehandler::Filehandler(const char* filename) {
 }
 
 
-/**
- * Checks whether a file exists or not.
- *
- * @return true if the file exists, false otherwise.
- */
 inline bool Filehandler::exists() {
     struct stat buffer;
     return (stat (filename, &buffer) == 0);
 }
 
-/**
- * Reads "bytes" bytes from a stream, starting at byte "position".
- *
- * @param position  The starting position of the pointer
- * @param bytes     The number of bytes that should be read
- * @return a pointer to the first byte of a char array containing the requested bytes
- */
+
 char* Filehandler::read(const unsigned int position, const unsigned char bytes) {
     char *buffer = new char[bytes];
 
@@ -41,13 +25,7 @@ char* Filehandler::read(const unsigned int position, const unsigned char bytes) 
     return buffer;
 }
 
-/**
- * Reads "bytes" bytes from a stream, starting at byte "position".
- *
- * @param position  The starting position of the pointer
- * @param bytes     The number of bytes that should be read
- * @return a pointer to the first byte of a char array containing the requested bytes
- */
+
 char* Filehandler::read(const unsigned int position, enum std::_Ios_Seekdir way, const unsigned char bytes) {
     char *buffer = new char[bytes];
 
@@ -57,9 +35,7 @@ char* Filehandler::read(const unsigned int position, enum std::_Ios_Seekdir way,
     return buffer;
 }
 
-/**
- * Class destructor, closes the filestream if open.
- */
+
 Filehandler::~Filehandler() {
     if(stream.is_open())
         stream.close();
