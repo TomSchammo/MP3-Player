ab7bbfcd52ac2151f0af3d383d35c5bf022363f9
Tom Schammo <tom@tomschammo.com>
2021-01-30 06:37:33 +0100

    Added documentation and some minor changes
    + Added documentation
    + Added variable for file offset to start of mp3 data
    * Changed variable naming (m prefix before member variables)
    * Capitalized S in class name

--- include/song.hpp
+++ include/song.hpp
@@ -13,20 +13,40 @@
 #include <cstdint>
 #include <string>
 
-class song
+
+/**
+ * Class wrapper for song data.
+ *
+ * This class contains necessary information about a song like metadata,
+ * a filepath, an offset to the start of the audio data, etc.
+ *
+ * Member variables:
+ *  m_title:       The song title
+ *  m_album:       The name of the album
+ *  m_artist:      The name of the artist
+ *  m_genre:       The name of the genre that the song belongs to
+ *  m_release:     The year of when the song was released
+ *  m_duration:    The song duration
+ *  m_path:        The path to the MP3 file
+ *  m_audio_start: An offset to the start of the actual audio data
+ */
+class Song
 {
 public:
-    std::string title = "Unknown Title";
-    std::string album = "Unknown Album";
-    std::string artist = "Unknown Artist";
-    std::string genre = "Unknown Genre";
-    std::uint16_t release = 0;
-    std::uint16_t duration = 0;
-    std::string path;
+    std::string m_title = "Unknown Title";
+    std::string m_album = "Unknown Album";
+    std::string m_artist = "Unknown Artist";
+    std::string m_genre = "Unknown Genre";
+    std::uint16_t m_release = 0;
+
+    std::uint16_t m_audio_start = 0;
+    std::uint16_t m_duration = 0;
+
+    std::string m_path;
 
 public:
-    song ();
-    virtual ~song ();
+    Song();
+    virtual ~Song ();
 
 };
 
