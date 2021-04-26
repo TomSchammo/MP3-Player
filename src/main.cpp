f38957975ce33df13fef54939ab0611948d2ad9a
Tom Schammo <tom@tomschammo.com>
2021-04-26 14:52:13 +0200

    Using std::chrono to profile program execution time

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <song.hpp>
 #include <id3.hpp>
+#include <chrono>
 
 
 int main(int arc, char *agrv[])
@@ -11,11 +12,19 @@ int main(int arc, char *agrv[])
 
         std::string filename = agrv[1];
 
+        auto start = std::chrono::high_resolution_clock::now();
+
         Song song(filename);
 
         readID3(song);
 
+        auto stop = std::chrono::high_resolution_clock::now();
+
         song.print();
+
+        auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(stop - start);
+
+        std::cout << "Execution took " << duration.count() << " milliseconds" << std::endl;
 
     }
 
