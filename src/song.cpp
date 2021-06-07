298507807b542b614475825f4170253a81478107
Tom Schammo <tom@tomschammo.com>
2021-06-07 12:55:12 +0200

    Added function to write image to file
    I added a function that allowes me to write images (album) cover to a
    file to test things

--- src/song.cpp
+++ src/song.cpp
@@ -1,10 +1,26 @@
 #include <song.hpp>
+#include <fstream>
 #include <iostream>
 
 
 Song::Song(const std::string& t_path) : m_path(t_path) {
     // TODO log debug (or info?)
     std::cout << "Created song object for file: " << t_path << std::endl;
+}
+
+void write_img(Song &song) {
+
+    for (auto art : song.m_art) {
+
+        std::ofstream stream;
+        stream.open(song.m_title + ".png", std::ios::binary | std::ios::out);
+        stream.write(art.m_data->data(), art.m_data->size());
+        stream.close();
+
+
+        // size is not what is supposed to be
+        std::cout << "size of pic_data: " << art.m_data->size() << std::endl;
+    }
 }
 
 void Song::print() {
@@ -19,6 +35,9 @@ void Song::print() {
     std::cout << "\n----- Audio information -----\n" << std::endl;
     std::cout << "Audio start: " << m_audio_start << std::endl;
     std::cout << "Audio duration: " << m_delay << std::endl;
+
+
+    write_img(*this);
 }
 
 Song::~Song() {
