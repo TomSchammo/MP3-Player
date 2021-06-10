f28fe75f6f0e7fa579d0c8bdc4fbd9817afce94b
Tom Schammo <tom@tomschammo.com>
2021-06-10 14:41:30 +0200

    Added logging
    Added file that I use for my log functions

---
+++ include/log.hpp
@@ -0,0 +1,43 @@
+#ifndef LOG_HPP
+#define LOG_HPP
+
+#include <experimental/source_location>
+#include <fmt/core.h>
+
+
+//    std::cout << "\033[31mred text\n" << std::endl;
+//    std::cout << "\033[33myellow text\n" << std::endl;
+//    std::cout << "\033[37mwhite text\n" << std::endl;
+//    std::cout << "\033[34mblue text\n" << std::endl;
+//    std::cout << "\033[0mdefault text\n" << std::endl;
+// https://stackoverflow.com/questions/4053837/colorizing-text-in-the-console-with-c
+
+class log {
+
+public:
+    static inline void info(const std::string& message, const std::experimental::source_location& location = std::experimental::source_location::current()) {
+        fmt::print("\033[37m[INFO]:[{}:{}]:[{}] {}\n",
+                   location.file_name(), location.line(), location.function_name(), message);
+    }
+
+
+    static inline void debug(const std::string& message, const std::experimental::source_location& location = std::experimental::source_location::current()) {
+        fmt::print("\033[34m[DEBUG]:[{}:{}]:[{}] {}\n",
+                   location.file_name(), location.line(), location.function_name(), message);
+    }
+
+
+    static inline void warn(const std::string& message, const std::experimental::source_location& location = std::experimental::source_location::current()) {
+        fmt::print("\033[33m[WARN]:[{}:{}]:[{}] {}\n",
+                   location.file_name(), location.line(), location.function_name(), message);
+    }
+
+
+    static inline void error(const std::string& message, const std::experimental::source_location& location = std::experimental::source_location::current()) {
+        fmt::print("\033[31m[ERROR]:[{}:{}]:[{}] {}\n",
+                   location.file_name(), location.line(), location.function_name(), message);
+    }
+};
+
+
+#endif /* ifndef LOG_HPP */
