257038a15a13112cb92955220d4758c357e35aa3
Tom Schammo <tom@tomschammo.com>
2021-06-11 14:39:55 +0200

    Added possibility for multiple arguments
    One can now test the application with more than one mp3 file at a time
    by providing multiple files as arguments

--- src/main.cpp
+++ src/main.cpp
@@ -2,34 +2,38 @@
 #include <string>
 #include <song.hpp>
 #include <id3.hpp>
-#include <chrono>
+// #include <chrono>
 
 
-int main(int arc, char *agrv[])
+int main(int arc, char* agrv[])
 {
 
     if (arc > 1) {
 
-        std::string filename = agrv[1];
+        for(int i = 1; i < arc; i++) {
+            std::string filename = agrv[i];
 
-        auto start = std::chrono::high_resolution_clock::now();
+            // auto start = std::chrono::high_resolution_clock::now();
 
-        Song song(filename);
+            Song song(filename);
 
-        readID3(song);
+            ID3::readID3(song);
 
-        auto stop = std::chrono::high_resolution_clock::now();
+            // auto stop = std::chrono::high_resolution_clock::now();
 
-        song.print();
+            song.print();
 
-        auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(stop - start);
+            // auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(stop - start);
 
-        std::cout << "Execution took " << duration.count() << " milliseconds" << std::endl;
+            // std::cout << "Execution took " << duration.count() << " milliseconds" << std::endl;
+
+        }
+
 
     }
 
     else {
-        std::cerr << "You need to provide a filename!" << std::endl;
+        std::cerr << "You need to provide at least one filename!" << std::endl;
     }
 
 
